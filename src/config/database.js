49b3dff316b1a2d10514c7068285cc995c5b3f41
savio591 <savio591@hotmail.com>
2021-07-10 11:05:25 -0300

    [MCE/Oboarding/Backend] - Feats: Db MySql para PgSQL(Elephant)

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
-  dialect: 'mysql',
-  host: 'localhost',
-  username: 'root',
-  // password: 'root',
-  database: 'onboarding_nodejs',
+  dialect: 'postgres',
+  host: 'batyr.db.elephantsql.com',
+  username: 'euqpxrvx',
+  password: '4iVP8UfRrg4aa6KrshPHse2DJ6gIGXFY',
+  database: 'euqpxrvx',
   define: {
     timestamps: true,
   },
